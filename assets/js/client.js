cb84f3e4d49e444a761122458c339597f039ce6f
Uri Gorelik <ugorelik@teldio.com>
2017-11-12 23:55:21 -0500

    Added localStorage support for clientId (#11)
    A client's ID will now be saved in local storage. Refreshing the page as
    the same client will no longer increment the "folk(s) waiting" number.

--- assets/js/client.js
+++ assets/js/client.js
@@ -1,3 +1,16 @@
+const L_ID = 'client_id'
+
+function getId() {
+  let id;
+  id = window.localStorage.getItem(L_ID)
+
+  if (!id) {
+    id = generateId()
+    window.localStorage.setItem(L_ID, id)
+  }
+  return id;
+}
+
 function generateId() {
   return Math.floor((Math.random() + 1) * 0x100000000).toString(16)
 }
@@ -5,7 +18,7 @@ function generateId() {
 export default class Client {
   constructor(channelId) {
     this.channelId = channelId;
-    this.id = generateId();
+    this.id = getId();
   }
 }
 
